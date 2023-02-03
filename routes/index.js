90bdd935a5dd08e3dbf1d3d9abb130b45622e62f
agu <iba.agustin@gmail.com>
2023-02-03 16:41:02 -0300

    initial skeleton, modified view of index file, added title and dummy messages

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  { text: "Hi there!", user: "Amando", added: new Date() },
+  { text: "Hello world!", user: "Charlie", added: new Date() },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini messageboard", messages: messages });
+});
+
+module.exports = router;
